3bb1d557cee3e02cd09e37e8054e0e724b7972ee
NateTheDev1 <46858886+NateTheDev1@users.noreply.github.com>
2020-02-02 15:15:31 -0500

    Added some styling to index

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './NoteList.css';
 
 export default class NoteList extends Component {
 	componentDidMount() {}
@@ -8,9 +9,11 @@ export default class NoteList extends Component {
 		const notes = Object.values(this.props.notes);
 		console.log(notes);
 		return notes.map((n) => (
-			<div key={n._id}>
+			<div key={n._id} className="LinkDiv">
 				<h2>
-					<Link to={`/notes/${n._id}`}>{n.title}</Link>
+					<Link to={`/notes/${n._id}`} className="Links">
+						<span className="NoteLinkText">{n.title}</span>
+					</Link>
 					{/* <a href={`/notes/${n._id}`}>{n.title}</a> */}
 				</h2>
 			</div>
